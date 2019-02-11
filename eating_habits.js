9676b8259651f479597a4947e2bf232fb9386bc3
Aidan Curtis <>
2019-02-11 14:37:49 -0600

    foursquare eating habits

--- eating_habits.js
+++ eating_habits.js
@@ -1,56 +1,98 @@
-var LOC_QID = "5c26a2d5aa840d2aff351434"
-var RADIUS_THRESH = 0.0002
+const MC = Questions[0]
+const LOC = Questions[1]
+const FD = Questions[2]
 
-deactivate([{_id: LOC_QID}], Users)
-activate([{_id: LOC_QID}], Users, {})
+const WAIT_DURATION = 15
+const MAX_QUESTION = 5
+
+function get_location_from_answers(answers){
+	locations = []
+	if(answers != undefined){
+		for(var i = 0; i<answers.length; i+=1){
+			if(answers[i].answerData != undefined && answers[i].answerData.latitude != undefined){
+				return answers[i]
+			}
+		}
+	}
+	return undefined
+}
+
+function get_name_from_venues(venues){
+	return venues.map(function(venue){return venue.name}).join()
+}
+
 for(var u_idx = 0; u_idx<Users.length; u_idx+=1){
+
+	var date = new Date()
+	var hour = date.toLocaleString('en-US', {hour: '2-digit',  hour12: false, timeZone: 'America/Chicago' })
+	var minute = date.toLocaleString('en-US', {minute: '2-digit',  timeZone: 'America/Chicago' })
 	var User = Users[u_idx]
-	var uid = Users[u_idx]._id
-	var Question = Questions[1]
+
+	if(hour == 9 && minute == 0){
+		send_notification("Make sure you add your breakfast to the food diary", User)
+	}
+	if(hour == 12 && minute == 0){
+		send_notification("Make sure you add your lunch to the food diary", User)
+	}
+	if(hour == 18 && minute == 0){
+		send_notification("Make sure you add your dinner to the food diary", User)
+	}
+
 	var new_store = {}
 	if(Store != undefined){
 		new_store = Store
 	}
-	if(Answers[uid] != null){
-		if(new_store[uid] == null){
-			new_store[uid] = {}
-		}
-		check_store = Answers[uid][0].answerData 
-		parameters = {
-			location: [parseFloat(check_store['latitude']), parseFloat(check_store['longitude'])],
-			rankby: "distance",
-			type: "food"
-		}
-		google_places.placeSearch(parameters, function (error, response) {
-			if (error) {
-				add_to_log(JSON.stringify(error))
-				throw error;
-			}
-			if(response.results.length>0) {
-				if (response.results[0].types.includes("food")) {
-					// module.exports = response.result
-					lat1 = response.results[0].geometry.location['lat']
-					lng1 = response.results[0].geometry.location['lng']
-					lat2 = parseFloat(check_store['latitude'])
-					lng2 = parseFloat(check_store['longitude'])
-					var lat_dist_sq = Math.abs(lat1-lat2)**2
-					var lng_dist_sq = Math.abs(lng1-lng2)**2
-					var radius = Math.sqrt(lat_dist_sq+lng_dist_sq)
 
-					if(new_store[uid][response.results[0].name] == undefined){
-						new_store[uid][response.results[0].name] = 1
-					} else if(radius<=RADIUS_THRESH){
-						new_store[uid][response.results[0].name] += 1
-						if(new_store[uid][response.results[0].name] == 15){
-							send_notification("Did you eat at "+response.results[0].name+" ?", User)
-							activate([Question], [User], {restaurant: response.results[0].name})
-						}
+	if(new_store[User._id] == null){
+		new_store[User._id] = {}
+		activate([FD], User, {})
+		activate([LOC], User, {})
+	}
+
+	if(Answers[User._id] != null){
+		var location = get_location_from_answers(Answers[User._id])
+		if(location != undefined) {
+			var check_store = location.answerData 
+			var loc = {
+				"lat": parseFloat(check_store['latitude']), 
+				"long": parseFloat(check_store['longitude'])
+			}
+			var params = {
+				radius: "50", //meters
+				categoryIds: ["4d4b7105d754a06374d81259"]
+			}
+			Foursquare.Venues.searchLocation(loc, params, "", (function(){
+				var cUser = User;
+				return function(err, response){
+					if (err) {
+						add_to_log(JSON.stringify(error))
+						throw err;
 					}
+					if(response.venues.length>0) {
+						venue_name = get_name_from_venues(response.venues)
+						if(new_store[cUser._id][venue_name] == undefined){
+							new_store[cUser._id][venue_name] = 1
+						} else {
+							new_store[cUser._id][venue_name] += 1
+							if(new_store[cUser._id][venue_name] == WAIT_DURATION){
+								restaurants = {}
+								for(var i = 0; i<Math.min(response.venues.length, MAX_QUESTION); i+=1){
+									restaurants["restaurant"+(i+1)] = response.venues[i].name
+								}
+								add_to_log(JSON.stringify(restaurants))
+								send_notification("Did you eat at one of these restaurants", cUser)
+								activate([MC], [cUser], restaurants)
+							}
+						}
+						add_to_log(JSON.stringify(new_store))
+					} 
 					store(new_store)
-					add_to_log(JSON.stringify(new_store))
 				}
-			} 
-			
-		});
+			})());
+		} else {
+			store(new_store)
+		}
+	} else {
+		store(new_store)
 	}
 }
